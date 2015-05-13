8a67457c359eba22b09a176fb69fea7bc89b010b
Mike McNeil <mike@balderdash.co>
2015-05-13 09:47:09 -0500

    Cleaned up a bit, added notes.

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ var Machine = require('machine');
  *
  * Build a conventional controller action (i.e. route handling function)
  * from a machine definition.  This wraps the machine in a function which
- * negotiates exits to the appropriate response method (e.g. res.serverError)
+ * negotiates exits to the appropriate response method (e.g. res.negotiate)
  * and passes in all of the request parameters as inputs, as well as a few
- * other useful scope variables including:
+ * other useful env variables including:
  *  • req
  *  • res
- *  • sails (if it exists)
  *
  * @param  {Object} machineDef
  * @return {Function}
@@ -35,25 +34,8 @@ module.exports = function machineAsAction(opts) {
     machineDef = opts.machine;
   }
 
-  // Build machine, applying defaults
-  var wetMachine = Machine.build(_.extend({
-    identity: machineDef.friendlyName||'anonymous-action',
-    inputs: {},
-    exits: {
-      success: {
-        description: 'Done.'
-      },
-      error: {
-        description: 'Unexpected error occurred.'
-      }
-    },
-    fn: function (inputs, exits){
-      exits.error(new Error('Not implemented yet!'));
-    }
-  },machineDef||{}));
 
-
-  return function _requestHandler(req, res, next) {
+  return function _requestHandler(req, res) {
 
     if (!req.allParams) {
       throw new Error('Currently, `machine-as-action` requires `req.allParams()` to exist (i.e. a Sails.js app with the request hook enabled)');
@@ -65,20 +47,49 @@ module.exports = function machineAsAction(opts) {
       throw new Error('Currently, `machine-as-action` requires `res.json()` to exist (i.e. a Sails.js or Express app)');
     }
 
-    // Configure inputs using request parameters
-    var liveMachine = wetMachine(req.allParams());
+    // TODO: ....be smart about upstreams here....
+
+    // Build machine, applying defaults
+    var wetMachine = Machine.build(_.extend({
+      identity: machineDef.friendlyName||'anonymous-action',
+      inputs: {},
+      exits: {
+        success: {
+          description: 'Done.'
+        },
+        error: {
+          description: 'Unexpected error occurred.'
+        }
+      },
+      fn: function (inputs, exits){
+        exits.error(new Error('Not implemented yet!'));
+      }
+    },machineDef||{}));
+
+    // Build input configuration for machine using request parameters
+    var inputConfiguration = _.extend({}, req.allParams());
+    var liveMachine = wetMachine.configure(inputConfiguration);
     // Provide `env.req` and `env.res`
     liveMachine.setEnvironment({
       req: req,
       res: res
     });
     // Now run the machine, proxying exits to the response.
-    liveMachine.exec({
+    return liveMachine.exec({
       error: function (err){
         return res.negotiate(err);
       },
       success: function (result){
         // TODO: handle other types of result data
+        // try {
+        // if (machineDef.exits.success.example === '*') {
+        //   // ...be smart about streams here...
+        // }
+        // catch (e){return res.negotiate(e);}
+
+        if (_.isUndefined(result)) {
+          return res.send(200);
+        }
         return res.json(result);
       }
     });
