caaae4f68fa0f691e70364c58df00aed41de88e0
Mike McNeil <mike@balderdash.co>
2015-05-18 11:18:28 -0500

    More tests

--- test/sanity.test.js
+++ test/sanity.test.js
@@ -1,9 +1,10 @@
+var util = require('util');
 var testRoute = require('./helpers/test-route.helper');
 
 
 
 
-testRoute('sanity test', {
+testRoute('sanity check', {
   machine: {
     inputs: {},
     exits: {},
@@ -22,7 +23,27 @@ testRoute('sanity test', {
 });
 
 
-testRoute('sanity test w/ params', {
+
+testRoute('if exit def + compatible output example is specified, result should be sent as the response body', {
+  machine: {
+    inputs: {},
+    exits: {
+      success: {
+        example: 'some string'
+      }
+    },
+    fn: function (inputs, exits) {
+      return exits.success('hello world!');
+    }
+  }
+}, function (err, resp, body, done){
+  if (err) return done(err);
+  if (body !== 'hello world!') return done(new Error('should have gotten "hello world!" as the response body, but instead got: '+util.inspect(body)));
+  return done();
+});
+
+
+testRoute('if input def + compatible input examples are specified, parameters should be provided as inputs', {
   machine: {
     inputs: {
       x: {
@@ -30,7 +51,11 @@ testRoute('sanity test w/ params', {
         required: true
       }
     },
-    exits: {},
+    exits: {
+      success: {
+        example: 'some string'
+      }
+    },
     fn: function (inputs, exits) {
       return exits.success(inputs.x);
     }
@@ -40,5 +65,36 @@ testRoute('sanity test w/ params', {
   }
 }, function (err, resp, body, done){
   if (err) return done(err);
+  if (body !== 'hello world!') return done(new Error('should have gotten "hello world!" as the response body, but instead got: '+util.inspect(body)));
+  return done();
+});
+
+
+
+
+testRoute('ignore extra parameters', {
+  machine: {
+    inputs: {
+      x: {
+        example: 'hi',
+        required: true
+      }
+    },
+    exits: {
+      success: {
+        example: 'some string'
+      }
+    },
+    fn: function (inputs, exits) {
+      return exits.success(inputs.y);
+    }
+  },
+  params: {
+    x: 'some value for x',
+    y: 'some value for y'
+  }
+}, function (err, resp, body, done){
+  if (err) return done(err);
+  if (body !== '') return done(new Error('should have gotten "" as the response body, but instead got: '+util.inspect(body)));
   return done();
 });
