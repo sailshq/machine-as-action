fef1c648004e6254ef3bab6a8a8452db8e17352f
Mike McNeil <mike@balderdash.co>
2016-10-02 22:30:14 -0500

    Expand double-wrap test to be more thorough.

--- test/double-wrap.test.js
+++ test/double-wrap.test.js
@@ -4,14 +4,16 @@ var testRoute = require('./util/test-route.util');
 
 
 
-testRoute('double-wrapping should fail', {
+testRoute('double-wrapping should fail when passing in an already-converted-machine-which-is-now-an-action as `machine`', {
+
   machine: asAction({
     inputs: {},
     exits: {},
     fn: function (inputs, exits) {
       return exits.success();
     }
-  }),
+  })
+
 }, function (err, resp, body, done){
   if (err) {
     assert.equal(err.code, 'E_DOUBLE_WRAP');
@@ -22,3 +24,19 @@ testRoute('double-wrapping should fail', {
 
 
 
+
+
+
+testRoute('double-wrapping should fail when passing in an already-converted-machine-which-is-now-an-action at the top level', asAction({
+  inputs: {},
+  exits: {},
+  fn: function (inputs, exits) {
+    return exits.success();
+  }
+}), function (err, resp, body, done){
+  if (err) {
+    assert.equal(err.code, 'E_DOUBLE_WRAP');
+    return done();
+  }
+  return done(new Error('Double-wrapped machine-as-action should have failed!'));
+});
